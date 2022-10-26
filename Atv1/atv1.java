22ab0def6b9683e0783c4c47ebe0cdece93a4c92
icaroparente <111507558+icaroparente@users.noreply.github.com>
2022-10-26 11:11:46 -0300

    Update atv1.java

--- Atv1/atv1.java
+++ Atv1/atv1.java
@@ -6,7 +6,10 @@ public class atv1 {
 
 	public static void main(String[] args) {
 		/*
-		//*****************QUESTÃO 1*****************
+		1. O custo de um carro novo ao consumidor é a soma do custo de fábrica com a porcentagem do distribuidor e dos impostos 
+		(aplicados ao custo de fábrica). Supondo que o percentual do distribuidor seja de 28% e os impostos de 45%, escrever um 
+			algoritmo para ler o custo de fábrica de um carro, calcular e escrever o custo final ao consumidor.*/
+		
 		float carro, distribuidor, imposto, vFinal;
 		
 		Scanner ler = new Scanner(System.in);
@@ -21,8 +24,13 @@ public class atv1 {
 		System.out.printf("Valor final: R$ %.2f", vFinal);
 		
 		ler.close();
-		*/
 		/*
+		2. Faça um algoritmo para ler: a descrição do produto (nome), a quantidade adquirida e o preço unitário. Calcular e escrever o total 
+		(total = quantidade adquirida * preço unitário), o desconto e o total a pagar (total a pagar = total - desconto), sabendo-se que:
+		- Se quantidade <= 5 o desconto será de 2%
+		- Se quantidade > 5 e quantidade <=10 o desconto será de 3%
+		- Se quantidade > 10 o desconto será de 5%
+		*/
 		//*****************QUESTÃO 2*****************
 		String nome;
 		float quantidade, preco, total;
@@ -50,8 +58,10 @@ public class atv1 {
 		}
 		
 		ler.close();
-		*/
+		
 		/*
+		3. Faça um algoritmo que leia 20 números e, ao final, escreva quantos estão entre 0 e 100, quantos estão entre 101 e 200 e quantos são maiores de 200.
+		*/
 		//*****************QUESTÃO 3*****************
 		int i, j, par=0, numero[];
 		
@@ -71,8 +81,10 @@ public class atv1 {
 		System.out.printf("Quantos número pares? %d", par);
 		
 		ler.close();
-		*/
+		
 		/*
+		4. Escreva um algoritmo que leia uma sequência de números do usuário e realize a soma desses números. Encerre a execução quando um número negativo for digitado.
+		*/
 		//*****************QUESTÃO 4*****************
 		int numero[], i, soma=0;
 		
@@ -93,25 +105,23 @@ public class atv1 {
 		System.out.printf("Soma = %d", soma);
 		
 		ler.close();
-		
+		/*
+		5. Reescreva os códigos aplicando funções
 		*/
-		//*****************QUESTÃO5*****************
+		//****************QUESTÃO5*****************
 		/*
 		//Função1
 		System.out.println("***Calculadora de custo***");
 		custo();
-		*/
-		/*
+		
 		//Função2
 		System.out.println("***Calculadora de desconto***");
 		desconto();
-		*/
-		/*
+		
 		//Função3
 		System.out.println("***Verificar números pares***");
 		pares();
-		*/
-		/*
+		
 		//Função4
 		System.out.println("***Cauular números >0***");
 		calculo();
